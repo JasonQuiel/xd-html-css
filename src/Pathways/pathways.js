7f2c18b3481784def3791e0971108ef5d620bcf6
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-11-15 15:45:13 +1300

    Pathway /grid

--- src/Pathways/pathways.js
+++ src/Pathways/pathways.js
@@ -1,3 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvD1vrBaoUghuGea3x2UyMDFXzeAinxsw",
+  authDomain: "test-685ee.firebaseapp.com",
+  projectId: "test-685ee",
+  storageBucket: "test-685ee.appspot.com",
+  messagingSenderId: "209567890273",
+  appId: "1:209567890273:web:0392f24b9e807a2129e320"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 // Side Navigation Function
 $('.canvas-toggle').hide();
 
